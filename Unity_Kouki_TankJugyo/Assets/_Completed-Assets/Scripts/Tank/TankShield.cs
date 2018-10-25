16a0035b78a2cb957f3d05ea1df52e6393b57d4c
KPC-017A1353\k017A1353 <kbtit56.gmail.com>
2018-10-25 14:46:03 +0900

    コミット

---
+++ Unity_Kouki_TankJugyo/Assets/_Completed-Assets/Scripts/Tank/TankShield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShield : MonoBehaviour {
+    public GameObject shield;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButton("Shield1"))
+        {
+            shield.SetActive(true);
+        }
+        else
+        {
+            shield.SetActive(false);
+        }
+	}
+
+    public bool IsShield()
+    {
+        return Input.GetButton("Shield1");
+    }
+}
